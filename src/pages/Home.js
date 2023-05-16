c31cf833806666703de6c291ce80177f644a48ec
sk1264 <samanthakaras1@gmail.com>
2023-05-16 17:34:26 -0400

    Incorporated Zelda font.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './styles.css';
 
 function Home(props) {
   const centerStyle = {
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily: 'ZeldaFont'
   };
 
   return <h1 style={centerStyle}>Felled Monsters of Hyrule</h1>;
