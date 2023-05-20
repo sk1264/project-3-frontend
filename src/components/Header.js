fbfccdad50b71a06dbdd928a4a5a71481cff9a47
flat1ander <thematthewlane@gmail.com>
2023-05-20 14:43:29 -0400

    Linked model to front end.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,60 +5,33 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">&#129321;</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-  
-  export default Header;
+  return (
+    <Nav className="justify-content-center" activeKey="/home">
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">&#129321;</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </Nav>
+  );
+}
 
-
-
-
-
-// function Header(props) {
-//   //inline style for the nav tag
-//   const navStyle = {
-//     display: "flex",
-//     justifyContent: "space-around",
-//     border: "3px solid black",
-//     padding: "8px",
-//     width: "90%",
-//     margin: "auto",
-//   };
-
-//   return (
-//     <header>
-//       <h1>Zelda App</h1>
-//       <nav style={navStyle}>
-//         <Link to="/">
-//           <div>HOME</div>
-//         </Link>
-//         </nav>
-//     </header>
-//   );
-// }
-
-// export default Header;
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import './Home.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { monstersLoader } from '../loaders';
 
-function Home(props) {
+function Home() {
   const centerStyle = {
     textAlign: 'center',
-    fontFamily: 'ZeldaFont'
   };
 
-  return (
-    <>
-      <h1 style={centerStyle}>Felled Monsters of Hyrule</h1>
-      <Row xs={1} md={3} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col key={idx} className="mb-4">
+  const zeldaFontStyle = {
+    fontFamily: 'ZeldaFont',
+  };
+
+  const [monstersData, setMonstersData] = useState([]);
+
+  useEffect(async () => {
+    const monsters = await monstersLoader();
+    setMonstersData(monsters);
+  }, []);
+
+  const monstersMarkup = (
+    <div style={centerStyle}>
+      <h1 style={zeldaFontStyle}>Felled Monsters of Hyrule</h1>
+      <Row xs={1} sm={2} md={3} lg={4} className="g-4 justify-content-center" style={{ margin: '2 auto' }}>
+        {monstersData.map((monster, index) => (
+          <Col key={index} className="mb-4">
             <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Img variant="top" src={monster.image} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{monster.name}</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
+                  {monster.description}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="success">More details</Button>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>{monster.difficulty}</ListGroup.Item>
+                <ListGroup.Item>{monster.tips}</ListGroup.Item>
+                <ListGroup.Item>{monster.date}</ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
-    </>
+    </div>
   );
+
+  return monstersData.length > 0 ? monstersMarkup : <div>Loading</div>;
 }
 
 export default Home;
