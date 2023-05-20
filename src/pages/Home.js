d34e36c8f1755e4bbdbae26add822e996461c129
sk1264 <samanthakaras1@gmail.com>
2023-05-20 11:45:52 -0400

    Background, card changes, working on centering.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,36 +3,57 @@ import './styles.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import './Home.css';
 
 function Home(props) {
   const centerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
     textAlign: 'center',
-    fontFamily: 'ZeldaFont'
+    backgroundImage: 'url("https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77700703052.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    padding: '20px',
+  };
+
+  const zeldaFontStyle = {
+    fontFamily: 'ZeldaFont',
   };
 
   return (
-    <>
-      <h1 style={centerStyle}>Felled Monsters of Hyrule</h1>
-      <Row xs={1} md={3} className="g-4">
+    <div style={centerStyle}>
+      <h1 style={zeldaFontStyle}>Felled Monsters of Hyrule</h1>
+      <Row xs={1} sm={2} md={3} lg={4} className="g-4 justify-content-center" style={{ margin: '2 auto' }}>
         {Array.from({ length: 4 }).map((_, idx) => (
           <Col key={idx} className="mb-4">
             <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Img variant="top" src="https://botw-compendium.herokuapp.com/api/v2/entry/blue_moblin/image" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Blue Moblin</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
+                  These heavyweight monsters can be found all over Hyrule. They're much tougher than their standard counterparts and often have much stronger weapons equipped.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="success">Go somewhere</Button>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
-    </>
+    </div>
   );
 }
 
